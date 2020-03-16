b9fd7d692a0fa66b6245450dea4e0f70c0a07ec1
sroot <1153475545@qq.com>
2020-03-16 21:24:19 +0800

    优化目录

--- __tests__/common/common.test.js
+++
@@ -1,5 +0,0 @@
-import printMe from '../../src/js/common/common'
-
-test('printMe function', () => {
-  expect(printMe()).toBe(console.log('================ This is a js webpack template.================='))
-})
